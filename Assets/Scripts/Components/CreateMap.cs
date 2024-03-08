21714c3f63cbdf3b09811b161a0789b04af52776
Suke-H <hondii1997@gmail.com>
2024-03-09 00:12:22 +0900

    feat: #25 Objectをグリッド配置
    close #25

--- Assets/Scripts/Components/CreateMap.cs
+++ Assets/Scripts/Components/CreateMap.cs
@@ -7,11 +7,13 @@ public class CreateMap : MonoBehaviour
 	[SerializeField] private int columns;
 	[SerializeField] private int rows;
 	[SerializeField] private Transform entirePlaneTransform;
+	[SerializeField] private MapTile[] Objects;
 
 	private float gridSize;
 
 	private List<List<GridSquare>> gridMap;
 	private List<List<string>> bookMap;
+	private List<List<int>> objectMap;
 
 	void Start()
 	{
@@ -25,17 +27,29 @@ public class CreateMap : MonoBehaviour
 		bookMap = new List<List<string>>();
 		bookMap.Add(new List<string> { "", "", "", "", "", "", "", "", "", "" });
 		bookMap.Add(new List<string> { "", "", "", "", "", "", "", "", "", "" });
-		bookMap.Add(new List<string> { "", "", "", "", "", "", "", "", "", "" });
 		bookMap.Add(new List<string> { "", "私", "は", "目", "を", "覚", "ま", "し", "た", "" });
-		bookMap.Add(new List<string> { "", "", "", "", "", "", "", "", "", "" });
-		bookMap.Add(new List<string> { "", "", "", "", "", "", "", "", "", "" });
 		bookMap.Add(new List<string> { "", "こ", "こ", "は", "ど", "こ", "だ", "ろ", "う", "" });
 		bookMap.Add(new List<string> { "", "", "", "", "", "", "", "", "", "" });
 		bookMap.Add(new List<string> { "", "", "", "", "", "", "", "", "", "" });
 		bookMap.Add(new List<string> { "", "", "", "", "", "", "", "", "", "" });
+		bookMap.Add(new List<string> { "", "", "", "", "", "", "", "", "", "" });
+		bookMap.Add(new List<string> { "", "", "", "", "", "", "", "", "", "" });
+		bookMap.Add(new List<string> { "", "", "", "", "", "", "", "", "", "" });
+
+
+		objectMap = new List<List<int>>();
+		objectMap.Add(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+		objectMap.Add(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+		objectMap.Add(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+		objectMap.Add(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+		objectMap.Add(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+		objectMap.Add(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+		objectMap.Add(new List<int> { 0, 0, 1, 0, 0, 0, 0, 2, 0, 0 });
+		objectMap.Add(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+		objectMap.Add(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+		objectMap.Add(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
 
 		GenerateGrid();
-
 	}
 
 	void GenerateGrid()
@@ -51,6 +65,20 @@ public class CreateMap : MonoBehaviour
 				gridSquare.name = $"{z}_{x}";
 				gridSquare.WriteText(bookMap[z][x]);
 				gridMap.Add(new List<GridSquare>());
+
+				if (objectMap[z][x] != 0)
+				{
+					MapTile _object = Instantiate(Objects[objectMap[z][x] - 1], new Vector3(-offset + x * gridSize, 0, offset - z * gridSize), Quaternion.identity);
+					// 親オブジェクトをこのスクリプトがアタッチされているオブジェクトにする
+					_object.transform.parent = this.transform;
+
+					var spriteRenderer = _object.SpriteRenderer;
+					float height = spriteRenderer.bounds.size.y;
+					// Y方向にSpriteの高さの半分だけ上げる
+					_object.transform.position += new Vector3(0, height / 2, 0);
+					Debug.Log("height = " + height);
+
+				}
 			}
 		}
 	}
